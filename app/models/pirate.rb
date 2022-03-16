c72cfbe7dc631cad957c8c7a7fc47636c77b380c
Peyton <pekellogg@hotmail.com>
2022-03-15 23:05:32 -0700

    complete

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_reader :name, :weight, :height
+    def initialize(params)
+        @name, @weight, @height = params[:name], params[:weight], params[:height]
+        @@all << self
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        all.clear
+    end
+
+    attr_reader :name, :type, :booty
+    def initialize(params)
+        @name, @type, @booty = params[:name], params[:type], params[:booty]
+        @@all << self
+    end
 end
